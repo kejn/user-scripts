c3367c56adfcd40756eaf3f1190628b891992ac2
Kejn <kaen04@gmail.com>
2017-07-24 22:59:40 +0200

    Create Packtpub_Claim_Free_Book.user.js

---
+++ Packtpub_Claim_Free_Book.user.js
@@ -0,0 +1,21 @@
+// ==UserScript==
+// @id           Packtpub_Claim_Free_Book@https://github.com/kejn/user-scripts
+// @name         Claim free book @packtpub.com
+// @namespace    https://www.packtpub.com/
+// @version      0.1
+// @description  Clicks on the 'Claim free book' button
+// @author       kejn
+// @homepage     https://github.com/kejn/user-scripts/tree/master
+// @homepageURL  https://github.com/kejn/user-scripts/tree/master
+// @downloadURL  https://github.com/kejn/user-scripts/raw/master/Packtpub_Claim_Free_Book.user.js
+// @updateURL    https://github.com/kejn/user-scripts/raw/master/Packtpub_Claim_Free_Book.user.js
+// @supportURL   https://github.com/kejn/user-scripts/issues
+// @include      https://www.packtpub.com/packt/offers/free-learning
+// @version      1.0.0
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+    $('#free-learning-claim').click();
+})();
