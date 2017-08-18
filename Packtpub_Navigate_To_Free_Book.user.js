95933951ebd07d639502f75eeb074f4ad10cc8f4
Kejn <kaen04@gmail.com>
2017-08-18 09:25:06 +0200

    Widened the search scope

--- Packtpub_Navigate_To_Free_Book.user.js
+++ Packtpub_Navigate_To_Free_Book.user.js
@@ -2,7 +2,7 @@
 // @id           Packtpub_Navigate_To_Free_Book@https://github.com/kejn/user-scripts
 // @name         Navigate to free books @packtpub.com
 // @namespace    https://github.com/kejn/user-scripts
-// @version      1.0.0
+// @version      1.1.0
 // @description  Navigates to the daily free book page
 // @author       kejn
 // @homepage     https://github.com/kejn/user-scripts/tree/master
@@ -16,7 +16,7 @@
 
 (function() {
     'use strict';
-    var links = $('.hero-blocks').first().find('a');
+    var links = $('a');
     for(var i=0; i < links.length; ++i) {
         if(links[i].href && links[i].href.indexOf('free-learning') >= 0) {
             window.location.href = links[i].href;
