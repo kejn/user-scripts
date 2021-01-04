90f8672542cc498f10de80dab48c2ee52f336c91
Kejn <kaen04@gmail.com>
2021-01-04 10:45:22 +0100

    Update Outlook_sidebar_remove.user.js

--- Outlook_sidebar_remove.user.js
+++ Outlook_sidebar_remove.user.js
@@ -1,16 +1,21 @@
 // ==UserScript==
 // @name         Outlook sidebar remove
 // @namespace    http://tampermonkey.net/
-// @version      0.1
+// @version      0.2
 // @description  try to take over the world!
 // @author       kejn
-// @match        https://outlook.live.com/mail/**/*
+// @match        https://outlook.live.com/mail*
 // @grant        none
 // ==/UserScript==
 
 (function() {
     'use strict';
-    setTimeout(function(){
-       document.querySelector('i[data-icon-name="OutlookLogo"]').parentElement.parentElement.parentElement.remove();
-    }, 4000);
+    var tryme = setInterval(function(){
+        var elem = document.querySelector('i[data-icon-name="OutlookLogo"]');
+        if (elem) {
+            elem.parentElement.parentElement.parentElement.remove();
+            clearInterval(tryme);
+        }
+        console.log('see this?');
+    }, 1000);
 })();
